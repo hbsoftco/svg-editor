bbe4ff376c57e2a14098ebeec396b4dabd60fa1f
Hossein <hossein.bajan@gmail.com>
2024-04-13 18:12:15 +0330

    Add canvas base file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { fabric } from 'fabric';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'svg-editor';
+  title = 'my-app';
+
+  ngOnInit() {
+    const canvas = new fabric.Canvas('myCanvas');
+
+    let rectangle = new fabric.Rect({
+      left: 10,
+      top: 10,
+      fill: 'red',
+      width: 60,
+      height: 70,
+    });
+
+    canvas.add(rectangle);
+  }
 }
